cad55ced25c2558f1bf4bbb46de28976e458c4cf
uerbzr <nigel.sibbert@gmail.com>
2023-03-17 23:11:53 +0000

    Add project files.

---
+++ csharp-template.Test/CoreTests.cs
@@ -0,0 +1,21 @@
+﻿using csharp_template.Main;
+using NUnit.Framework;
+
+namespace csharp_template.Test
+{
+    [TestFixture]
+    public class CoreTests
+    {
+        private Core _core;
+        public CoreTests()
+        {
+            _core = new Core();
+        }
+        [Test]
+        public void Test()
+        {
+            Assert.Pass();
+        }
+
+    }
+}
---
+++ csharp-template.Test/ExtensionTests.cs
@@ -0,0 +1,19 @@
+﻿using csharp_template.Main;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_template.Test
+{
+    public class ExtensionTests
+    {
+        private Extension _extension;
+        public ExtensionTests()
+        {
+            _extension = new Extension();
+
+        }
+    }
+}
