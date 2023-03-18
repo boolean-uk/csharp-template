2d5f6047dc6182c71338a2994b90c43decfffff6
uerbzr <nigel.sibbert@gmail.com>
2023-03-18 11:39:14 +0000

    Updated project

---
+++ Boolean.CSharp.Test/CoreTests.cs
@@ -0,0 +1,13 @@
+﻿using Boolean.CSharp.Main;
+
+namespace Boolean.CSharp.Test
+{
+    public class CoreTests
+    {
+        private CoreTests _core;
+        public CoreTests()
+        {
+            Core
+        }
+    }
+}
