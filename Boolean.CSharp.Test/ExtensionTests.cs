856e48145b3d369ab2eade0528a36205f37632e5
uerbzr <nigel.sibbert@gmail.com>
2023-03-21 18:04:45 +0000

    completed

--- Boolean.CSharp.Test/ExtensionTests.cs
+++ Boolean.CSharp.Test/ExtensionTests.cs
@@ -1,4 +1,5 @@
 ﻿using Boolean.CSharp.Main;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,12 +8,18 @@ using System.Threading.Tasks;
 
 namespace Boolean.CSharp.Test
 {
+    [TestFixture]
     public class ExtensionTests
     {
         private Extension _extension;
         public ExtensionTests()
         {
             _extension = new Extension();
+        }
+        [Test]
+        private void TestExtension()
+        {
+
         }
     }
 }
